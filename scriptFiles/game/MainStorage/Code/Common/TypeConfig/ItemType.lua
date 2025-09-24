ded04fb32952cbc14b78d44c4ceab3ea2cf7aaf6
XF <1131680660@qq.com>
2025-09-24 17:13:59 +0800

    修改物品配置

--- scriptFiles/game/MainStorage/Code/Common/TypeConfig/ItemType.lua
+++ scriptFiles/game/MainStorage/Code/Common/TypeConfig/ItemType.lua
@@ -5,89 +5,60 @@ local gg                = require(MainStorage.Code.Untils.MGlobal)    ---@type g
 
 -- ItemType class
 ---@class ItemType:Class
----@field name string 物品名称
----@field description string 物品描述
----@field detail string 详细描述
----@field icon string 物品图标
----@field quality string 物品品质
----@field power number 战力
----@field enhanceRate number 强化倍率
----@field enhanceMaterials table<string, number> 强化素材
----@field enhanceMaterialRate number 强化材料增加倍率
+---@field name string 名字
+---@field displayName string 显示名称
+---@field description string 描述
+---@field icon string 图标
+---@field quality string 品级
+---@field extraPower number 额外战力
 ---@field maxEnhanceLevel number 最大强化等级
----@field attributes table<string, number> 属性
----@field tags table<string, boolean> 标签
----@field collectionReward string 图鉴完成奖励ID
----@field collectionRewardAmount number 图鉴完成奖励数量
----@field collectionAdvancedRewardAmount number 图鉴高级完成奖励数量
----@field equipmentSlot number 装备格子ID
----@field evolveTo string 可进阶为的物品ID
----@field evolveMaterials table<string, number> 进阶材料
----@field boundTags table 获得词条
----@field sellPrice number 售出价格
----@field showInBag boolean 是否在背包里显示
+---@field maxDurability number 最大耐久度
+---@field maxStack number 最大叠加数量
 ---@field isStackable boolean 是否可堆叠
----@field maxStack number 最大堆叠数量
----@field gainSound string 获得音效
----@field cancelGain boolean 是否取消获得
----@field gainCommands table 获得时执行的指令
----@field useCommands table 使用时执行的指令
----@field decomposeResult table 分解产物
+---@field itemTypeStr string 类型
+---@field attackType string 攻击类型
+---@field isMoney boolean 是否货币
+---@field equipmentConfig string|nil 装备配置
+---@field canHold boolean 可手持
+---@field equippable boolean 是否可装备
+---@field color table 颜色
+---@field useCommands table 使用执行指令
+---@field gainCommands table 获取执行指令
+ 
 ---@field New fun( data:table ):ItemType
 local ItemType = ClassMgr.Class("ItemType")
 
 function ItemType:OnInit(data)
-    -- 基本信息
-    self.name = data["名字"] or "Unknown Item"
-    self.description = data["描述"] or ""
-    self.detail = data["详细属性"] or ""
-    self.icon = data["图标"] or ""
-    self.quality = data["品级"] or "普通"
-    self.power = data["战力"] or 0
+	-- 基本信息（仅使用最新配置字段）
+	self.name = data["名字"] or "Unknown Item"
+	self.displayName = data["显示名称"] or self.name
+	self.description = data["描述"] or ""
+	self.icon = data["图标"] or ""
+	self.quality = data["品级"] or "N"
+	self.extraPower = data["额外战力"] or 0
 
-    -- 强化
-    self.enhanceRate = data["强化倍率"] or 0
-    self.enhanceMaterials = data["强化素材"] or {}
-    self.enhanceMaterialRate = data["强化材料增加倍率"] or 0
-    self.maxEnhanceLevel = data["最大强化等级"] or 0
-    
-    -- 属性
-    self.attributes = data["属性"] or {}
-    self.tags = data["标签"] or {}
-    
-    -- 使用与获得
-    self.canAutoUse = data["可自动使用"] or true
-    self.useCommands = data["使用执行指令"] or {}
-    self.useCooldown = data["使用冷却"] or -1
-    self.useConsume = data["使用消耗"] or 1
-    self.gainCommands = data["获得执行指令"] or {}
-    self.gainSound = data["获得音效"] or ""
-    self.cancelGain = data["取消获得物品"] or false
+	-- 强化/耐久与叠加
+	self.maxEnhanceLevel = data["最大强化等级"] or 0
+	self.maxDurability = data["最大耐久度"] or 0
+	self.maxStack = data["最大叠加数量"] or 1
+	self.isStackable = (self.maxStack or 1) > 1
 
-    -- 收集
-    self.collectionReward = data["图鉴完成奖励"]
-    self.collectionRewardAmount = data["图鉴完成奖励数量"] or 0
-    self.collectionAdvancedRewardAmount = data["图鉴高级完成奖励数量"] or 0
-    
-    -- 装备
-    self.equipmentSlot = data["装备格子"] or -1
-    
-    -- 进阶
-    self.evolveTo = data["可进阶为"]
-    self.evolveMaterials = data["进阶材料"] or {}
-    
-    -- 其他
-    self.boundTags = data["获得词条"] or {}
-    self.decomposeResult = data["分解可得"] or {}
-    self.sellableTo = data["可售出为"]
-    self.sellPrice = data["售出价格"] or 0
-    
-    -- 显示与货币
-    self.showInBag = data["在背包里显示"] or true
-    self.itemTypeStr = data["物品类型"]
-    self.isMoney = (self.itemTypeStr == "货币")
-    self.isStackable = data["是否可堆叠"] or false
-    self.maxStack = data["最大数量"] or 1
+	-- 类型与战斗属性
+	self.itemTypeStr = data["类型"] or ""
+	self.attackType = data["攻击类型"] or "无"
+	self.isMoney = (self.itemTypeStr == "货币")
+
+	-- 装备/持有
+	self.equipmentConfig = data["装备配置"]
+	self.canHold = data["可手持"] or false
+	self.equippable = data["是否可装备"] or false
+
+	-- 颜色
+	self.color = data["颜色"] or { r = 255, g = 255, b = 255, a = 255 }
+
+	-- 指令
+	self.useCommands = data["使用执行指令"] or {}
+	self.gainCommands = data["获取执行指令"] or {}
 end
 
 function ItemType:GetToStringParams()
@@ -113,17 +84,6 @@ function ItemType:CreateCompleteItemData(amount, enhanceLevel, quality)
     }
 end
 
--- function ItemType:ToItem(count)
---     local Item = require(MainStorage.code.server.bag.Item) ---@type Item
---     local item = Item.New()
---     item:Load({
---         uuid = gg.create_uuid('item'),
---         itype = self,
---         amount = count,
---         el = 0,
---         quality = ""
---     })
---     return item
--- end
+
 
 return ItemType
